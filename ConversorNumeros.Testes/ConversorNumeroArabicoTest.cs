64bc945f7e5bbe97c965e18b16964659cdbbe3e1
Alexandre Rech <alexandre.rech@hotmail.com>
2022-06-10 16:48:32 -0300

    Adicionado as classes para a conversão de numeros arabicos

---
+++ ConversorNumeros.Testes/ConversorNumeroArabicoTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversorNumeros.Testes
+{
+    [TestClass]
+    public class ConversorNumeroArabicoTest
+    {
+        [TestMethod]
+        public void MyTestMethod()
+        {
+
+        }
+    }
+}
